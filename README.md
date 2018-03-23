bfa5dcb5a502c41d1086af6d95ad51c7ecc964b4
Ruoran Wang <ruoran0502@gmail.com>
2018-03-22 18:58:12 -0700

    correct the code for question 2 move Creat View to README.md

--- README.md
+++ README.md
@@ -10,9 +10,42 @@ There're three tables in the database. The *authors* table includes information 
 
 In order to get the result, some views have been created.
 
-For the first question, the view that has been created is called `sliced_path` which contains a column of parts of log.path used to compare articles.slug later.
+For the first question, the view that has been created is called `sliced_path` which contains a column of parts of log.path used to compare articles.slug later:
 
-For the second question, join articles and authors tables to create a view named `authors_articles` with author and their articles. Create a view named `date_fail` with date and fail response on that date. Create a view named `date_total_response` with date and total response on that day. Create view named `date_fail_rate` with date and fail responses on that day. Create another view called `date_percent` with column date and numeric. The numeric column is about fail_rate multiply 100.0 then rounded.
+```python
+create view sliced_path (sliced_name) as select substring(log.path,10) from log;
+```
+
+For the second question, create a view named `article_view` as:
+
+```python
+create view article_view as select title,author,count(*) as views
+from articles,log where log.path like concat('%',articles.slug)
+group by articles.title,articles.author
+order by views desc;
+```
+
+ Create a view named `date_fail` with date and fail response on that date. Create a view named `date_total_response` with date and total response on that day. Create view named `date_fail_rate` with date and fail responses on that day. Create another view called `date_percent` with column date and numeric. The numeric column is about fail_rate multiply 100.0 then rounded:
+
+```python
+ create view date_fail as select time::date as date, count(status) as fails
+ from log where status != '200 OK' group by time::date;
+```
+
+```python
+create view date_fail_rate as select date_total_response.date as date,
+date_fail.fails/date_total_response.total_response::float as fail_rate
+from date_total_response, date_fail
+where date_total_response.date=date_fail.date
+order by fail_rate DESC;
+```
+
+```python
+create view date_percent as select date,
+cast(fail_rate*100.0 as decimal(10,2))
+from date_fail_rate;
+```
+
 
 ### Prerequisites
 
--- databaselog.py
+++ databaselog.py
@@ -5,10 +5,6 @@ import psycopg2
 # What are the most popular three articles of all time?
 query_1_title = ("What are the most popular three articles of all time?")
 query_1 = (
-    """create view sliced_path (sliced_name) as
-    select substring(log.path,10) from log;
-    """
-
     """ select articles.title, count(sliced_path.sliced_name)as num
     from articles, sliced_path where articles.slug = sliced_path.sliced_name
     group by articles.title order by num DESC;
@@ -17,38 +13,14 @@ query_1 = (
 # Who are the most popular article authors of all time?
 query_2_title = ("who are the most popular article authors of all time?")
 query_2 = (
-    """create view popular_articles as select articles.title as articles_name,
-    count(sliced_path.sliced_name)as num from articles, sliced_path
-    where articles.slug = sliced_path.sliced_name
-    group by articles.title order by num DESC;
-    """
-    """create view authors_articles as
-    select articles.title as article, authors.name as author
-    from articles, authors
-    where articles.author = authors.id;
-    """
-    """select authors_articles.author, count (popular_articles.num) as total_views
-    from authors_articles, popular_articles
-    where authors_articles.article = popular_articles.articles_name
-    group by authors_articles.author order by total_views DESC;
+    """select authors.name,sum(article_view.views) as views
+    from article_view,authors where authors.id = article_view.author
+    group by authors.name order by views desc;
     """)
 
 # On which days did more than 1% of requests lead to errors
 query_3_title = ("On which days did more than 1% of requests lead to errors?")
 query_3 = (
-    """create view date_fail as select time::date as date, count(status) as fails
-    from log where status != '200 OK' group by time::date;
-    """
-    """create view date_fail_rate as select date_total_response.date as date,
-    date_fail.fails/date_total_response.total_response::float as fail_rate
-    from date_total_response, date_fail
-    where date_total_response.date=date_fail.date
-    order by fail_rate DESC;
-    """
-    """create view date_percent as select date,
-    cast(fail_rate*100.0 as decimal(10,2))
-    from date_fail_rate;
-    """
     """select date, numeric from date_percent where numeric >= 1.00;
     """)
 
@@ -59,6 +31,7 @@ def connect(database_name="news"):
         db = psycopg2.connect("dbname={}".format(database_name))
         cursor = db.cursor()
         return db, cursor
+
     except:
         print("Unable to connect to database.")
 
@@ -72,7 +45,7 @@ def get_query_results(query):
 
 
 def print_query_results(query_results):
-    print (query_results[1])
+    print(query_results[1])
     for index, results in enumerate(query_results[0]):
         print(
             index+1, "-", results[0],
@@ -80,9 +53,10 @@ def print_query_results(query_results):
 
 
 def print_error_results(query_results):
-    print (query_results[1])
+    print(query_results[1])
     for results in query_results[0]:
         print(results[0], "-", str(results[1]) + " % errors")
+
 
 if __name__ == '__main__':
     # store query results
